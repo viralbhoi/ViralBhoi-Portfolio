046736c6642a814e0bd77594163ec5aedc2eda24
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2025-12-03 23:50:33 +0530

    Update resume link in DesktopScreen component

--- src/pages/DesktopScreen.jsx
+++ src/pages/DesktopScreen.jsx
@@ -124,7 +124,7 @@ function DesktopScreen() {
                         label="Resume"
                         onClick={() => {
                             window.open(
-                                "https://drive.google.com/file/d/1vTlD3Llfdw7CWrz-U5zwTJ0MHmGdVjpx/view?usp=sharing",
+                                "https://drive.google.com/file/d/1L2S3Ig8cnsQDgiT_cGAQYRDANch2MyMt/view?usp=sharing",
                                 "_blank",
                                 "noopener,noreferrer"
                             );
