ce7639102e048e7c9eefe6f88d5f7f16438487cd
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2025-12-07 20:55:23 +0530

    Update resume link in MobileDesktop component

--- src/components/Desktop/MobileDesktop.jsx
+++ src/components/Desktop/MobileDesktop.jsx
@@ -63,7 +63,7 @@ function MobileDesktop() {
                                     name !== "Resume"
                                         ? handleAppClick(name)
                                         : window.open(
-                                              "https://drive.google.com/file/d/1L2S3Ig8cnsQDgiT_cGAQYRDANch2MyMt/view?usp=sharing",
+                                              "https://drive.google.com/file/d/19CyPSjWQfq-tepRLU4j3oKPPC9QR5d4c/view?usp=sharing",
                                               "_blank",
                                               "noopener,noreferrer"
                                           );
